3e0a283dd10bb366fafe05f3df8c7fd57f3d7ffe
Ben Hanna <benpaulhanna@gmail.com>
2018-04-17 12:13:19 +0000

    Rewrote tests to be more well defined.

--- test/index.js
+++ test/index.js
@@ -6,39 +6,19 @@ const R = require('ramda');
 const { Injector, constant, service } = require('../');
 
 describe('Container', () => {
-  class A {
-    constructor(b) {
-      this.b = b;
-    }
-  }
+  const A = 'A';
   
   class B {
-    constructor(c) {
+    constructor(a, c) {
+      this.a = a;
       this.c = c;
     }
   }
   
-  class C {
-    constructor(d) {
-      this.d = d;
-    }
-  }
-  
-  const D = 'D';
-  
-  class E {
-    constructor(c, f) {
-      this.c = c;
-      this.f = f;
-    }
-  }
-  
-  class F {
-    constructor() {}
-  }
+  class C {}
 
   describe('Functional', function () {
-    const libraries = [
+    const composers = [
       {
         name: 'reduce',
         compose(...pipes) {
@@ -61,107 +41,92 @@ describe('Container', () => {
       }
     ];
   
-    libraries.forEach(function (library) {
-      describe(library.name, function () {
-        it(`should fail to resolve A when dependencies can't be resolved`, function () {
-          const container = library.compose(
-            service(A, [B.name])
-          )({});
-          
-          assert.throws(function () {
-            container.A;
-          }, Error);
-        });
-        
-        it(`should resolve A and inject dependencies`, function () {
-          const container = library.compose(
-            constant('D', D),
-            service(A, [B.name]),
-            service(B, [C.name]),
-            service(C, ['D'])
-          )({});
+    composers.forEach(function ({ name, compose }) {
+      describe(name, function () {
+        it('should resolve a constant', function () {
+          const container = compose(
+            constant('A', A)
+          )(Object.create(null));
           
           assert(container.A !== undefined, '\'A\' was undefined.');
-          assert(container.A.b !== undefined, '\A.b\' was undefined.');
-          assert.deepEqual(container.A.b, container.B, '\'B\' instances are not the same.');
+          assert.deepEqual(container.A, A, '\'A\' instances are not the same.');
         });
         
-        it(`should resolve E and inject dependencies`, function () {
-          const container = library.compose(
-            constant('D', D),
-            service(C, ['D'])
-          )({});
+        it('should resolve a service', function () {
+          const container = compose(
+            service(C, [])
+          )(Object.create(null));
           
-          assert(container.C !== undefined, '\'C\' was undefined.');
-          assert(container.C.d !== undefined, '\C.d\' was undefined.');
-          assert.deepEqual(container.C.d, container.D, '\'D\' instances are not the same.');
+          assert(container.C !== undefined, '\C\' was undefined.');
         });
         
-        it(`should resolve E and inject dependencies`, function () {
-          const container = library.compose(
-            constant('D', D),
-            service(A, [B.name]),
-            service(B, [C.name]),
-            service(C, ['D']),
-            service(E, [C.name, F.name]),
-            service(F, [])
-          )({});
+        it('should resolve a service with dependencies', function () {
+          const container = compose(
+            constant('A', A),
+            service(B, ['A', C.name]),
+            service(C, [])
+          )(Object.create(null));
           
-          assert(container.E !== undefined, '\'E\' was undefined.');
-          assert(container.E.c !== undefined, '\E.c\' was undefined.');
-          assert.deepEqual(container.E.c, container.C, '\'C\' instances are not the same.');
-          assert(container.E.f !== undefined, '\E.f\' was undefined.');
-          assert.deepEqual(container.E.f, container.F, '\'F\' instances are not the same.');
+          assert(container.B !== undefined, '\B\' was undefined.');
+          
+          assert(container.B.a !== undefined, '\B.a\' was undefined.');
+          assert.deepEqual(container.B.a, container.A, '\'A\' instances are not the same.');
+          
+          assert(container.B.c !== undefined, '\B.c\' was undefined.');
+          assert.deepEqual(container.B.c, container.C, '\'C\' instances are not the same.');
+        });
+        
+        it('should fail to resolve a service when dependencies can\'t be resolved', function () {
+          const container = compose(
+            service(B, ['A', C.name])
+          )(Object.create(null));
+          
+          assert.throws(function () {
+            container.B;
+          }, Error);
         });
       });
     });
   });
   
   describe('Injector', function () {
-    it(`should fail to resolve A when dependencies can't be resolved`, function () {
-      const { container } = new Injector().service(A, [B.name]);
-      
-      assert.throws(function () {
-        container.A;
-      }, Error);
-    });
-    
-    it(`should resolve A and inject dependencies`, function () {
+    it('should resolve a constant', function () {
       const { container } = new Injector()
-        .constant('D', D)
-        .service(A, [B.name])
-        .service(B, [C.name])
-        .service(C, ['D']);
+        .constant('A', A);
       
       assert(container.A !== undefined, '\'A\' was undefined.');
-      assert(container.A.b !== undefined, '\A.b\' was undefined.');
-      assert.deepEqual(container.A.b, container.B, '\'B\' instances are not the same.');
+      assert.deepEqual(container.A, A, '\'A\' instances are not the same.');
     });
     
-    it(`should resolve E and inject dependencies`, function () {
+    it('should resolve a service', function () {
       const { container } = new Injector()
-        .constant('D', D)
-        .service(C, ['D']);
+        .service(C, []);
       
-      assert(container.C !== undefined, '\'C\' was undefined.');
-      assert(container.C.d !== undefined, '\C.d\' was undefined.');
-      assert.deepEqual(container.C.d, container.D, '\'D\' instances are not the same.');
+      assert(container.C !== undefined, '\C\' was undefined.');
     });
     
-    it(`should resolve E and inject dependencies`, function () {
+    it('should resolve a service with dependencies', function () {
       const { container } = new Injector()
-        .constant('D', D)
-        .service(A, [B.name])
-        .service(B, [C.name])
-        .service(C, ['D'])
-        .service(E, [C.name, F.name])
-        .service(F, []);
+        .constant('A', A)
+        .service(B, ['A', C.name])
+        .service(C, []);
       
-      assert(container.E !== undefined, '\'E\' was undefined.');
-      assert(container.E.c !== undefined, '\E.c\' was undefined.');
-      assert.deepEqual(container.E.c, container.C, '\'C\' instances are not the same.');
-      assert(container.E.f !== undefined, '\E.f\' was undefined.');
-      assert.deepEqual(container.E.f, container.F, '\'F\' instances are not the same.');
+      assert(container.B !== undefined, '\B\' was undefined.');
+      
+      assert(container.B.a !== undefined, '\B.a\' was undefined.');
+      assert.deepEqual(container.B.a, container.A, '\'A\' instances are not the same.');
+      
+      assert(container.B.c !== undefined, '\B.c\' was undefined.');
+      assert.deepEqual(container.B.c, container.C, '\'C\' instances are not the same.');
+    });
+    
+    it('should fail to resolve a service when dependencies can\'t be resolved', function () {
+      const { container } = new Injector()
+        .service(B, ['A', C.name]);
+      
+      assert.throws(function () {
+        container.B;
+      }, Error);
     });
   });
 });
